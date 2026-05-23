12fb68ecebf32b3cdccedd5d52a9b2d1ca108053
lseep0 <wuntsumlazagna@gmail.com>
2026-05-23 19:54:17 +0100

    test done

---
+++ ClassLibrary/clsGame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsGame
+    {
+        public int GameId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public bool IsEarlyAccess { get; set; }
+    }
+}
---
+++ TestingGames/GameTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+
+namespace TestingGames
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsGame AGame = new clsGame();
+
+            Assert.IsNotNull(AGame);
+        }
+
+        [TestMethod]
+        public void TitlePropertyOK()
+        {
+            clsGame AGame = new clsGame();
+            string TestData = "Elden Ring";
+
+            AGame.Title = TestData;
+
+            Assert.AreEqual(AGame.Title, TestData);
+        }
+
+        [TestMethod]
+        public void PricePropertyOK()
+        {
+            clsGame AGame = new clsGame();
+            decimal TestData = 59.99m;
+
+            AGame.Price = TestData;
+
+            Assert.AreEqual(AGame.Price, TestData);
+        }
+    }
+}
