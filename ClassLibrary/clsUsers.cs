d072e6bdc06f2c9ce0105067224946c5efc1e041
Madalitso Mavindidze <P2833313@my365.dmu.ac.uk>
2026-05-26 10:46:43 +0100

    clsUsers class code

--- ClassLibrary/clsUsers.cs
+++ ClassLibrary/clsUsers.cs
@@ -8,6 +8,17 @@ namespace ClassLibrary
         private DateTime mJoinedDate;
         //privete data member for the User ID property
         private Int32 mUserID;
+        //private data member for active property
+        private bool mActive;
+        //private data member for the user name property
+        private string mUserName;
+        //private data member for the password property
+        private string mPassword;
+        //private data member for the user email property
+        private string mUserEmail;
+        //private data member for the user email verified property
+        private bool mUserEmailVerified;
+
         public int UserID 
         { 
             get
@@ -22,7 +33,17 @@ namespace ClassLibrary
                 
         }
 
-        public bool Active { get; set; }
+        public bool Active
+        {
+            get
+            {
+                return mActive;
+            }
+            set
+            {
+                mActive = value;
+            }
+        }
         public DateTime JoinedDate
         {
             get
@@ -37,16 +58,61 @@ namespace ClassLibrary
                 mJoinedDate = value;
             }
         }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string UserEmail { get; set; }
-        public bool UserEmailVerified { get; set; }
+        public string UserName 
+        {
+            get 
+            {
+                return mUserName;
+            }
+            set 
+            { 
+                mUserName = value;
+            } 
+        }
+        public string Password 
+        {
+            get 
+            {
+                return mPassword;
+            }
+            set 
+            { 
+                mPassword = value;
+            } 
+        }
+        public string UserEmail 
+        {
+            get 
+            {
+                return mUserEmail;
+            }
+            set 
+            {
+                mUserEmail = value;
+            } 
+        }
+        public bool UserEmailVerified 
+        {
+            get 
+            {
+                return mUserEmailVerified;
+            }
+            set 
+            {
+                mUserEmailVerified = value;
+            }
+        }
          
         public bool Find(int userID)
         {
             //set the private data member to the test data value;
             mUserID = 1;
             mJoinedDate = Convert.ToDateTime("23/12/2022");
+            mActive = true;
+            mUserName = "john pork";
+            mPassword = "1234";
+            mUserEmail = "johnpork@gmail.com";
+            mUserEmailVerified = true;
             //always return true
             return true;
         }
